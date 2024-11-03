84f0e8e62c0e5bcaddea2a4d5bae5c9827699220
ST3ERLING <121317030+ST3ERLING@users.noreply.github.com>
2024-11-03 20:23:55 +0100

    Spring Security , Jwt

---
+++ backend/src/main/java/com/mylibrary/backend/dtos/RegisterUserDto.java
@@ -0,0 +1,13 @@
+package com.mylibrary.backend.dtos;
+
+import lombok.Data;
+
+@Data
+public class RegisterUserDto {
+    private String email;
+
+    private String password;
+
+    private String fullName;
+
+}
---
+++ backend/src/main/java/com/mylibrary/backend/entitie/User.java
@@ -0,0 +1,80 @@
+package com.mylibrary.backend.entitie;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Table(name = "users")
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(unique = true, length = 100, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+}
