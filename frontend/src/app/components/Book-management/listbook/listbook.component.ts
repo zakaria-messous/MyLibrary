f5ee2b60d4cde83ba10d601a588b202e40de468a
Sarahbelaarabi <92765414+Sarahbelaarabi@users.noreply.github.com>
2024-11-08 22:53:55 +0100

    success run table prime ng

---
+++ frontend/src/app/components/Book-management/listbook/listbook.component.ts
@@ -0,0 +1,143 @@
+import { Component, OnInit } from '@angular/core';
+import { SidenavComponent } from "../../sidenav/sidenav.component";
+import { NavComponent } from "../../nav/nav.component";
+import { Product } from '../../../models/product';
+import { ImportsModule } from '../../../../imports';
+import { ProductService } from '../../../services/productService';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-listbook',
+  standalone: true,
+  imports: [SidenavComponent, NavComponent,ImportsModule
+    
+  ],
+  providers: [MessageService, ConfirmationService, ProductService],
+  templateUrl:'./listbook.component.html',
+  styleUrl: './listbook.component.css'
+})
+export class ListbookComponent implements OnInit{
+  productDialog: boolean = false;
+
+  products!: Product[];
+
+  product!: Product;
+
+  selectedProducts!: Product[] | null;
+
+  submitted: boolean = false;
+
+  statuses!: any[];
+
+  constructor(private productService: ProductService, private messageService: MessageService, private confirmationService: ConfirmationService) {}
+
+  ngOnInit() {
+      this.productService.getProducts().then((data) => (this.products = data));
+
+      this.statuses = [
+          { label: 'INSTOCK', value: 'instock' },
+          { label: 'LOWSTOCK', value: 'lowstock' },
+          { label: 'OUTOFSTOCK', value: 'outofstock' }
+      ];
+  }
+
+  openNew() {
+      this.product = {};
+      this.submitted = false;
+      this.productDialog = true;
+  }
+
+  deleteSelectedProducts() {
+      this.confirmationService.confirm({
+          message: 'Are you sure you want to delete the selected products?',
+          header: 'Confirm',
+          icon: 'pi pi-exclamation-triangle',
+          accept: () => {
+              this.products = this.products.filter((val) => !this.selectedProducts?.includes(val));
+              this.selectedProducts = null;
+              this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 });
+          }
+      });
+  }
+
+  editProduct(product: Product) {
+      this.product = { ...product };
+      this.productDialog = true;
+  }
+
+  deleteProduct(product: Product) {
+      this.confirmationService.confirm({
+          message: 'Are you sure you want to delete ' + product.name + '?',
+          header: 'Confirm',
+          icon: 'pi pi-exclamation-triangle',
+          accept: () => {
+              this.products = this.products.filter((val) => val.id !== product.id);
+              this.product = {};
+              this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Deleted', life: 3000 });
+          }
+      });
+  }
+
+  hideDialog() {
+      this.productDialog = false;
+      this.submitted = false;
+  }
+
+  saveProduct() {
+      this.submitted = true;
+
+      if (this.product.name?.trim()) {
+          if (this.product.id) {
+              this.products[this.findIndexById(this.product.id)] = this.product;
+              this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000 });
+          } else {
+              this.product.id = this.createId();
+              this.product.image = 'product-placeholder.svg';
+              this.products.push(this.product);
+              this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Created', life: 3000 });
+          }
+
+          this.products = [...this.products];
+          this.productDialog = false;
+          this.product = {};
+      }
+  }
+
+  findIndexById(id: string): number {
+      let index = -1;
+      for (let i = 0; i < this.products.length; i++) {
+          if (this.products[i].id === id) {
+              index = i;
+              break;
+          }
+      }
+
+      return index;
+  }
+
+  createId(): string {
+      let id = '';
+      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      for (var i = 0; i < 5; i++) {
+          id += chars.charAt(Math.floor(Math.random() * chars.length));
+      }
+      return id;
+  }
+
+  getSeverity(inventoryStatus: string | undefined): "success" | "warning" | "danger" | "info" {
+    switch (inventoryStatus) {
+        case 'In Stock':
+            return 'success';
+        case 'Low Stock':
+            return 'warning';
+        case 'Out of Stock':
+            return 'danger';
+        case 'unknown':
+            return 'info';
+        default:
+            return 'info'; // Return 'info' or any other fallback severity if the status is unrecognized
+    }
+}
+
+  
+  }
