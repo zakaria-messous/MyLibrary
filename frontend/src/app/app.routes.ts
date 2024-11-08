21d00cd61c781f70a7e02219f7ea2bab3352b978
meriamebibleghache <105889947+meriamebibleghache@users.noreply.github.com>
2024-11-08 14:30:42 +0000

    loginAndRegisterFront

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 // app.routes.ts
 import { Routes } from '@angular/router';
 import { DashbordAdminComponent } from './components/dashbord-admin/dashbord-admin.component';
-
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 export const routes: Routes = [
   { path: 'app-dashbord-admin', component: DashbordAdminComponent },
-  
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
