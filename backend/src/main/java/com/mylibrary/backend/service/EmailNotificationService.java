d19dba6a0fe8f410216595625434d184af1608f6
zakariamessous <bosszaki2001@gmail.com>
2024-12-01 13:25:32 +0100

    try ro fix pipline

--- backend/src/main/java/com/mylibrary/backend/service/EmailNotificationService.java
+++ backend/src/main/java/com/mylibrary/backend/service/EmailNotificationService.java
@@ -1,8 +1,5 @@
 package com.mylibrary.backend.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
